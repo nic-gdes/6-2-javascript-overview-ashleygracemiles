45aaf5e41b399b229e6ef2e81788b21c4066a616
Ashley Miles <agmiles@nic.edu>
2023-10-04 20:17:31 -0700

    final

--- javascript.js
+++ javascript.js
@@ -1,13 +1,52 @@
 // 1.1 Assignment: Where do you put JavaScript?
+alert("JavaScript is easy");
 
 // 1.2 Assignment: Variables
+var john_age = 25;
+var john_gender = 'male';
+var john_location = 'United Kingdom';
+
+console.log(john_age);
+console.log(john_gender);
+console.log(john_location);
 
 // 1.3 Assignment: Variable Declaration & Initialization
+var a = "23";
+var b = "Heyyyyyy!";
+var c = true;
+var d = [1, 2, 3];
+var e;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
 
 // 1.4 Assignment: Complete your statements
+x = 2;
+y = 3;
+
+z = x + y;
 
 // 1.5 Assignment: Brackets, Braces and Parentheses
+var a = function () {
+    alert('Statement 1');
+    alert('Statement 2');
+}
 
 // 1.6 Assignment: Data Types
+var f = "Ashley"; // object
+var g = 23; // number
+var h = "I like coffee"; // string, double quote
+
+console.log(f);
+console.log(g);
+console.log(h);
 
 // 1.7 Assignment: Practicing JavaScript basics
+var A = 4;
+var B = 3;
+var C = 5;
+
+console.log(B * A / 2);
